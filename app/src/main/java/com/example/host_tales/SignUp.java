c57b0b9628d043ddc4396c045c2c5b9c1d301664
amits <amitsharmaproxy@gmail.com>
2021-03-16 14:04:51 +0530

    Host tales..till login screen.

--- app/src/main/java/com/example/host_tales/SignUp.java
+++ app/src/main/java/com/example/host_tales/SignUp.java
@@ -4,25 +4,26 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.view.WindowManager;
 import android.widget.Button;
 
-public class Login extends AppCompatActivity {
+public class SignUp extends AppCompatActivity {
 
-    Button callSignUp;
+    Button callLogin;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
-        setContentView(R.layout.activity_login);
+        setContentView(R.layout.activity_sign_up);
 
-        callSignUp = findViewById(R.id.signup_loginpage_button);
-        callSignUp.setOnClickListener(new View.OnClickListener() {
+        callLogin = findViewById(R.id.login_signup_button);
+        callLogin.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(Login.this, SignUp.class);
+            public void onClick(View v) {
+                Intent intent = new Intent(SignUp.this,Login.class);
                 startActivity(intent);
                 finish();
             }

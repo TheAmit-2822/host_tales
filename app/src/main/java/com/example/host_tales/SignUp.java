898146312d03e677386a480c39a4cb70b862f565
amits <amitsharmaproxy@gmail.com>
2021-03-18 20:16:26 +0530

    Login Screen + Signup Screen with firebase.

--- app/src/main/java/com/example/host_tales/SignUp.java
+++ app/src/main/java/com/example/host_tales/SignUp.java
@@ -8,10 +8,19 @@ import android.util.Log;
 import android.view.View;
 import android.view.WindowManager;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import com.google.android.material.textfield.TextInputLayout;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class SignUp extends AppCompatActivity {
 
-    Button callLogin;
+    Button callLogin, regBtn; // back to login btn and storing data to firebase button.
+    TextInputLayout regName, regUsername, regEmail, regPhone, regPassword; // all input layout id hook in xml page.
+    FirebaseDatabase rootNode; //main root node
+    DatabaseReference reference;  //sub nodes of our database
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,7 +28,17 @@ public class SignUp extends AppCompatActivity {
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_sign_up);
 
+
+        //Hooks
         callLogin = findViewById(R.id.login_signup_button);
+        regName = findViewById(R.id.name);
+        regUsername = findViewById(R.id.username);
+        regEmail = findViewById(R.id.email);
+        regPhone = findViewById(R.id.phone);
+        regPassword = findViewById(R.id.password);
+        regBtn = findViewById(R.id.signup_signup_button);
+
+        //Return to Login Page
         callLogin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -28,5 +47,29 @@ public class SignUp extends AppCompatActivity {
                 finish();
             }
         });
+
+        //Store data to Firebase
+        regBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                rootNode = FirebaseDatabase.getInstance();
+                reference = rootNode.getReference("Users");
+
+
+                //Fetching all the  values
+                String name = regName.getEditText().getText().toString();
+                String username = regUsername.getEditText().getText().toString();
+                String email = regEmail.getEditText().getText().toString();
+                String phone = regPhone.getEditText().getText().toString();
+                String password = regPassword.getEditText().getText().toString();
+
+
+                UserHelperClass  helperClass = new UserHelperClass(name, username, email, phone, password);
+                reference.child(phone).setValue(helperClass);
+
+            }
+        });
+
     }
+
 }
